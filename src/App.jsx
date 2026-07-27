6035b3cd84cf2c4a0615c8c28a141a813fd2a6ff
Talal Hassan <aroobjan965@gmail.com>
2026-07-27 03:51:29 -0700

    Initial portfolio setup with React, Vite, and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar/Navbar.jsx"
+import Hero from "./components/Hero/Hero.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
