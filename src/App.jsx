20ab5c0c479d730445731b9bacd1706c39d61bc1
Talal Hassan <aroobjan965@gmail.com>
2026-07-27 04:19:27 -0700

    Create basic navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <>
       <Navbar />
-      <Hero />
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,17 @@
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-slate-900 text-white px-8 py-4 flex justify-between items-center">
+      <h1 className="text-2xl font-bold">
+        Talal.
+      </h1>
+
+      <ul className="flex gap-6">
+        <li className="cursor-pointer hover:text-blue-400">Home</li>
+        <li className="cursor-pointer hover:text-blue-400">About</li>
+        <li className="cursor-pointer hover:text-blue-400">Skills</li>
+        <li className="cursor-pointer hover:text-blue-400">Projects</li>
+        <li className="cursor-pointer hover:text-blue-400">Contact</li>
+      </ul>
+    </nav>
+  );
 }
