b80dd712f7db656a423470992e913aae3ec1ce0e
Talal Hassan <aroobjan965@gmail.com>
2026-07-29 03:39:40 -0700

    Portfolio ready for production

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -1,58 +1,33 @@
 import { Link } from "react-scroll";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo.svg";
 
 export default function Logo() {
   return (
     <Link
       to="home"
-      smooth={true}
+      smooth
       duration={500}
-      spy={true}
       offset={-80}
-      className="
-        flex
-        items-center
-        gap-3
-        cursor-pointer
-        select-none
-      "
+      className="flex items-center gap-3 cursor-pointer"
     >
-      {/* Logo */}
       <img
         src={logo}
-        alt="Talal Hassan Logo"
+        alt="Talal Hassan"
         className="
-          w-12
           h-12
-          object-contain
+          w-auto
           transition-transform
           duration-300
-          hover:scale-110
+          hover:scale-105
         "
       />
 
-      {/* Name */}
       <div className="leading-tight">
-        <h1
-          className="
-            text-xl
-            font-black
-            text-white
-            tracking-wide
-          "
-        >
-          Talal
-          <span className="text-blue-500">.</span>
+        <h1 className="text-xl font-black text-white">
+          Talal Hassan
         </h1>
 
-        <p
-          className="
-            text-xs
-            uppercase
-            tracking-[3px]
-            text-slate-400
-          "
-        >
+        <p className="text-[11px] uppercase tracking-[3px] text-slate-400">
           Front-End Developer
         </p>
       </div>
